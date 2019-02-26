e9da0fdfa8aae9c29a4dd7e4e1ddba73d6033e05
mateu <mateusz.rostalski@gmail.com>
2019-02-26 12:27:46 +0100

    start

---
+++ src/test/java/pl/mr/polkongres/KartaPrelegentaApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mr.polkongres;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KartaPrelegentaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
